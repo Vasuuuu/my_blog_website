c7a87df60a3dffbf0a5e64856b74ee2c68137125
Vasu <87427261+Vasuuuu@users.noreply.github.com>
2023-04-06 05:06:54 +0530

    Add files via upload

---
+++ blog/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+define('ROOT_URL','http://localhost/blog/');
+define('DB_HOST','localhost');
+define('DB_USER','manu');
+define('DB_PASS','1234');
+define('DB_NAME','blog');
